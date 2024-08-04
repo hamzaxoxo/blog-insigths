4e884e4f279f54d677e45f24ebfef9d8df1a113f
Hamza Malik <m.hamza@xeverse.io>
2024-08-05 00:39:02 +0500

    feat:commet

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,78 +1,78 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, "Please provide a username"],
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: [true, "Please provide a email"],
-        lowercase: true,
-        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please use a valid address'],
-        unique: true,
-        index: true
-    },
-    password: {
-        type: String,
-        required: [true, "Please provide a password"],
-    },
-    isVerfied: {
-        type: Boolean,
-        default: false,
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-    },
-    forgotPasswordToken: {
-        type: String,
-    },
-    forgotPasswordTokenExpiry: {
-        type: Date,
-    },
-    verifyToken: {
-        type: String,
-    },
-    verifyTokenExpiry: {
-        type: Date,
-    },
-    refreshToken: {
-        type: String,
-    },
-    last_login: {
-        type: Date,
-        default: Date.now(),
-    },
-    profile: {
-        name: {
-            type: String,
-        },
-        bio: {
-            type: String,
-        },
-        image: {
-            type: String,
-        },
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now(),
-    },
-})
+// const userSchema = new mongoose.Schema({
+//     username: {
+//         type: String,
+//         required: [true, "Please provide a username"],
+//         unique: true,
+//     },
+//     email: {
+//         type: String,
+//         required: [true, "Please provide a email"],
+//         lowercase: true,
+//         match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please use a valid address'],
+//         unique: true,
+//         index: true
+//     },
+//     password: {
+//         type: String,
+//         required: [true, "Please provide a password"],
+//     },
+//     isVerfied: {
+//         type: Boolean,
+//         default: false,
+//     },
+//     isAdmin: {
+//         type: Boolean,
+//         default: false,
+//     },
+//     forgotPasswordToken: {
+//         type: String,
+//     },
+//     forgotPasswordTokenExpiry: {
+//         type: Date,
+//     },
+//     verifyToken: {
+//         type: String,
+//     },
+//     verifyTokenExpiry: {
+//         type: Date,
+//     },
+//     refreshToken: {
+//         type: String,
+//     },
+//     last_login: {
+//         type: Date,
+//         default: Date.now(),
+//     },
+//     profile: {
+//         name: {
+//             type: String,
+//         },
+//         bio: {
+//             type: String,
+//         },
+//         image: {
+//             type: String,
+//         },
+//     },
+//     createdAt: {
+//         type: Date,
+//         default: Date.now(),
+//     },
+//     updatedAt: {
+//         type: Date,
+//         default: Date.now(),
+//     },
+// })
 
-userSchema.set('toJSON', {
-    transform: (doc, ret) => {
-        delete ret.password;
-        return ret;
-    }
-});
+// userSchema.set('toJSON', {
+//     transform: (doc, ret) => {
+//         delete ret.password;
+//         return ret;
+//     }
+// });
 
-const User = mongoose.models.users || mongoose.model("users", userSchema);
+// const User = mongoose.models.users || mongoose.model("users", userSchema);
 
-export default User;
+// export default User;
